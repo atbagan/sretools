c5656fcfd616890694251147f430be9187a301af
UTROUA1 <andrew.trousilbagan@pearson.com>
2022-02-19 10:44:33 -0600

    codedeploy

---
+++ cmd/codedeploy.go
@@ -0,0 +1,13 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var codedeployCmd = &cobra.Command{
+	Use:   "codedeploy",
+	Short: "CodeDeploy Commands",
+	Long:  "Commands related to CodeDeploy.",
+}
+
+func init() {
+	rootCmd.AddCommand(codedeployCmd)
+}
