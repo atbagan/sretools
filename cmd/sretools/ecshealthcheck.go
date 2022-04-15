b2270583edd13d98b7232d7aa097e6c29479bb5d
UTROUA1 <andrew.trousilbagan@pearson.com>
2022-04-15 17:50:03 -0500

    refactored

--- cmd/sretools/ecshealthcheck.go
+++ cmd/sretools/ecshealthcheck.go
@@ -1,5 +1,5 @@
 // Package sretools
-// Copyright (c) 2022, Andrew Bagan
+// Copyright (c) 2022, Andrew Bagan & WarrensBox
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
